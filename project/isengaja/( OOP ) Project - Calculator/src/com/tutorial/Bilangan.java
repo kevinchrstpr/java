6266a26383ab82633d85622dd15f4bd7fef460d8
Kevin Ope <41668520+kevinchrstpr@users.noreply.github.com>
2020-10-29 19:35:42 +0700

    Add files via upload

---
+++ project/isengaja/( OOP ) Project - Calculator/src/com/tutorial/Bilangan.java
@@ -0,0 +1,30 @@
+package com.tutorial;
+
+import java.util.*;
+
+class Bilangan {
+    Scanner inputUser = new Scanner(System.in);
+    int opsi;
+
+    Bilangan(int opsi){
+        this.opsi = inputUser.nextInt();
+    }
+}
+
+class Function {
+    Scanner inputUser = new Scanner(System.in);
+    double type1, type2;
+
+    Function(double type1, double type2){
+        this.type1 = inputUser.nextDouble();
+        this.type2 = inputUser.nextDouble();
+    }
+}
+
+class Operand {
+    double add, subs, multiply, divide;
+
+    Operand(double funcAdd, double funcSubs, double funcMultiply, double funcDivide){
+        this.add = 
+    }
+}
---
+++ project/isengaja/( OOP ) Project - Calculator/src/com/tutorial/Main.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
